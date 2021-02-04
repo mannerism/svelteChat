c6a9e8681596a4c4b2cc76d6e5466edab1e5e700
Juno Yu <jxy35@hitit.xyz>
2021-02-04 16:53:56 +0900

    first

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { FirebaseApp, User } from "sveltefire";
+  import firebase from "firebase/app";
+  import "firebase/firestore";
+  import "firebase/auth";
+  import AuthForm from "./components/AuthForm.svelte";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcEJKKwOBAgq8Rd2KgL-UJWkD6pxfcjq4",
+    authDomain: "svelte-chat-25a1f.firebaseapp.com",
+    projectId: "svelte-chat-25a1f",
+    storageBucket: "svelte-chat-25a1f.appspot.com",
+    messagingSenderId: "365895790533",
+    appId: "1:365895790533:web:c9220dae23c30f687d65ac",
+    measurementId: "G-GBRPRH31CQ",
+  };
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<main class="container section">
+  <FirebaseApp {firebase}>
+    <User let:user let:auth>
+      <div slot="signed-out">
+        <AuthForm {auth} />
+      </div>
+    </User>
+  </FirebaseApp>
+</main>
+
+<style>
+
+</style>
---
+++ src/components/AuthForm.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let auth;
+  let mode = "signIn";
+  let email = "";
+  let password = "";
+</script>
+
+<h1 class="title has-text-info has-text-centered">Svelte Chat</h1>
+<h2 class="title">{mode == "signIn" ? "Sign In" : "Sign Up"}</h2>
+<form>
+  <div class="field">
+    <label for="" class="label">Email</label>
+    <input type="email" class="input" required bind:value={email} />
+  </div>
+  <div class="field">
+    <label for="" class="label">Password</label>
+    <input type="password" class="input" required bind:value={password} />
+  </div>
+</form>
